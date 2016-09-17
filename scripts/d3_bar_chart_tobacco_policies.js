ba00b8b035a29cca965d111d95e989a6fe37ea5e
Igor Sudijovski <igorsudijovski@gmail.com>
2016-09-17 22:54:38 +0200

    Implemented tobacco polices bar chart

---
+++ scripts/d3_bar_chart_tobacco_policies.js
@@ -0,0 +1,104 @@
+(function() {
+  var tobbaco_url = "json_db/tobacco_policies.json";
+
+  var margin = {top: 20, right: 20, bottom: 70, left: 400},
+    width = 700 - margin.left - margin.right,
+    height = 800 - margin.top - margin.bottom;
+
+  var y_use = d3.scale.ordinal().rangeRoundBands([0, height], 0.1);
+
+  var x_use = d3.scale.linear().range([0, width]);
+
+  var xAxis_use = d3.svg.axis()
+    .scale(x_use)
+    .orient("bottom");
+  var yAxis_use = d3.svg.axis()
+    .scale(y_use)
+    .orient("left");
+
+
+  d3.json(tobbaco_url, function (error, tobaccoPolicies) {
+
+    var data = tobaccoPolicies.fact;
+    var notAplicable = ['Not applicable', 'Data not reported', 'None', 'Data not reported/not categorized'];
+
+    var policies = {};
+
+    for (var i = 0; i < data.length; i++) {
+      var notAplicableForList = false;
+      for (var n = 0; n < notAplicable.length; n++) {
+        if (data[i].Value == notAplicable[n]) {
+          notAplicableForList = true;
+        }
+      }
+      if (notAplicableForList) {
+        continue;
+      }
+      if (typeof policies[data[i].Value] === 'undefined') {
+        policies[data[i].Value] = 1;
+      } else {
+        policies[data[i].Value] = policies[data[i].Value] + 1;
+      }
+    }
+    var listPolicies = [];
+    for (var police in policies) {
+      if (policies.hasOwnProperty(police)) {
+        if (police.length < 60) {
+          listPolicies.push({"police": police, "value": policies[police]});
+        }
+      }
+    }
+    drawHorizontalBarGraph(listPolicies)
+  });
+
+  function drawHorizontalBarGraph(data) {
+
+    var svg = d3.select(".tobaccoPolicies").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+
+    x_use.domain([0, d3.max(data, function (d) {
+      return d.value;
+    })]);
+    y_use.domain(data.map(function (d) {
+      return d.police;
+    }));
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis_use)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)");
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis_use)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end");
+
+    svg.selectAll("bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .style("fill", "steelblue")
+      .attr("y", function (d) {
+        return y_use(d.police);
+      })
+      .attr("height", y_use.rangeBand())
+      .attr("x", 0)
+      .attr("width", function (d) {
+        return x_use(d.value);
+      });
+  }
+})();
---
+++ scripts/d3_bar_chart_tobacco_use.js
@@ -0,0 +1,132 @@
+(function() {
+  var tobbaco_url = "json_db/tobacco_use.json";
+
+  var margin = {top: 20, right: 20, bottom: 70, left: 40},
+    width = 600 - margin.left - margin.right,
+    height = 300 - margin.top - margin.bottom;
+
+  var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+  var y = d3.scale.linear().range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+  var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+  var europeData = [];
+
+
+  d3.json(tobbaco_url, function(error, tobbacoData) {
+
+    var data = tobbacoData.fact;
+    var macedonia = "Macedonia";
+    var filteredData = [];
+
+    var allCountries = [];
+    var countriesMap = {};
+
+    for (var i = 0; i < data.length; i++) {
+      if (data[i].dim.REGION === 'Europe' && data[i].dim.SEX == "Male") {
+        europeData.push(data[i]);
+        if (typeof countriesMap[data[i].dim.COUNTRY] === 'undefined') {
+          allCountries.push(data[i].dim.COUNTRY);
+          countriesMap[data[i].dim.COUNTRY] = 'inserted';
+        }
+        if (data[i].dim.COUNTRY == macedonia) {
+          filteredData.push(data[i]);
+        }
+      }
+    }
+    countriesMap = {};
+    allCountries.sort();
+    addCountriesToDropdown(allCountries);
+    filteredData.sort(compareYears);
+    drawGraph(filteredData);
+  });
+
+  function addCountriesToDropdown(countries) {
+    var dropDown = $('#addCountry');
+    for (var i = 0; i < countries.length; i++) {
+      dropDown.append('<li class="dropdown-item"><a onclick="clickDropDown(\'' + countries[i] +'\')">' + countries[i] + '</a></li>')
+    }
+  }
+
+  function clickDropDown(country) {
+    var filteredData = [];
+    $('#tobacoCountryName').text(country);
+    for (var i = 0; i < europeData.length; i++) {
+      if (europeData[i].dim.COUNTRY == country) {
+        filteredData.push(europeData[i]);
+      }
+    }
+    filteredData.sort(compareYears);
+    d3.select("svg").remove();
+    drawGraph(filteredData);
+
+  }
+
+  function drawGraph(data) {
+
+    var svg = d3.select(".tobbacoGraph").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+    data.forEach(function(d) {
+      d.graphYear = d.dim.YEAR;
+      d.graphValue = d.Value.replace(/ \[.*\]/gi, '');
+    });
+
+    x.domain(data.map(function(d) { return d.graphYear; }));
+    y.domain([0, d3.max(data, function(d) { return d.graphValue; })]);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+    svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end");
+
+    svg.selectAll("bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function(d) { return x(d.graphYear); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.graphValue); })
+      .attr("height", function(d) { return height - y(d.graphValue); });
+  }
+
+
+
+  function compareYears(a, b) {
+    var first = parseInt(a.dim.YEAR);
+    var second = parseInt(b.dim.YEAR);
+    if (first < second) {
+      return -1
+    }
+    if (first > second) {
+      return 1;
+    }
+    return 0;
+  }
+})();
