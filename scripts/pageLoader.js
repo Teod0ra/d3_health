420795fd559f6db7124e9ce209c71872fe307d81
Teodora <cvetanovska.teodora@gmail.com>
2016-09-17 14:21:21 +0200

    some name changes

---
+++ scripts/pageLoader.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Teodora on 17.09.2016.
+ */
+$(function(){
+
+    var lastSelectedItem = null;
+
+    $("#navbarHolder").load("./navbar.html");
+    $("#footerHolder").load("./footer.html");
+
+
+
+
+});
